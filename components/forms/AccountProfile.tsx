eee3ee798af02f50e3ab8893478bad5699cf727d
heywhyyou <havenoreasontolive@gmail.com>
2023-12-28 03:48:39 +0300

    style: add onboarding styles

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -155,7 +155,9 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
             </FormItem>
           )}
         />
-        <Button type="submit">Submit</Button>
+        <Button type="submit" className="bg-primary-500">
+          Submit
+        </Button>
       </form>
     </Form>
   );
